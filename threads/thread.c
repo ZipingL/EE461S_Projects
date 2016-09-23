dbfa06eb5151c6695f0aa26bc2545a77611cd277
kuthalam <mukundan@utexas.edu>
2016-09-23 19:21:43 +0100

    Resolved any errors related to the system calls. Still need to fix most of the errors that have to do with process.c. Especially during declaration of the esp_arg_ptrs array.

--- threads/thread.c
+++ threads/thread.c
@@ -98,9 +98,9 @@ thread_init (void)
   init_thread (initial_thread, "main", PRI_DEFAULT);
   initial_thread->status = THREAD_RUNNING;
   initial_thread->tid = allocate_tid ();
-  init_thread->fdtable[0] = STDIN;
-  init_thread->fdtable[1] = STDOUT;
-  init_thread->fdtable[2] = STDERR; /* Set up the file descriptor table as well */
+  initial_thread->fdtable[0] = 0;
+  initial_thread->fdtable[1] = 1;
+  initial_thread->fdtable[2] = 2; /* Set up the file descriptor table as well */
 }
 
 /* Starts preemptive thread scheduling by enabling interrupts.
